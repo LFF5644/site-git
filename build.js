2322dc7c3726be08d12d301be44c0c0abb90264c
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-03-16 22:36:31 +0100

    polyfills changebil

--- build.js
+++ build.js
@@ -22,9 +22,21 @@ try{
 }catch(e){}
 
 for(let item of build){
+	item={
+		type: null,
+		file: null,
+		language: "ECMASCRIPT3_STRICT",
+		polyfills: true,
+		...item,
+	};
 	if(item.type=="google-closure-compiler"){
 		console.log(`Compile ${item.file} to ${item.language}`);
-		console.log(execSync(`${item.type} --language_out "${item.language}" --js "${item.file}" --js_output_file "build/${item.file}"`).toString());
+		let cmd=item.type;
+		cmd+=` --js "${item.file}"`;
+		cmd+=` --js_output_file "build/${item.file}"`;
+		cmd+=` --language_out "${item.language}"`;
+		cmd+=item.polyfills?"":" --rewrite_polyfills false";
+		console.log(execSync(cmd).toString("utf-8"));
 	}else{
 		console.log("not allowed type "+item.type);
 	}
